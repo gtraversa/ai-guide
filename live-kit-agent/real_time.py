62b7f109df557163809c3dacc43fda91458e138f
Gianluca Traversa <gianlucatraversa@Mac.lan>
2025-07-09 13:17:12 -0400

    gpio test

--- live-kit-agent/real_time.py
+++ live-kit-agent/real_time.py
@@ -7,11 +7,25 @@ from livekit.plugins import (
     noise_cancellation,
 )
 from livekit.agents.llm import ChatContext
+import asyncio
+
+import RPi.GPIO as GPIO
+
+BUTTON = 17
+
+def button_callback(channel):
+    print("Button pressed!")
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Set up event detection
+GPIO.add_event_detect(BUTTON, GPIO.FALLING, callback=button_callback, bouncetime=200)
 
 load_dotenv()
 
 
-class Assistant(Agent):
+class AI_Guide(Agent):
     def __init__(self) -> None:
         super().__init__(instructions="You are a statue of Enzo Ferrari in a museum. Guests will be asking you questions regarding yourself," \
                                     " your life, the history of Ferrari. Guests will also be asking you general questions unrelated to yourself." \
@@ -30,26 +44,46 @@ class Assistant(Agent):
 
 
 async def entrypoint(ctx: agents.JobContext):
+
+    agent = AI_Guide()
   
     realtime_llm = openai.realtime.RealtimeModel(
         voice="ash",
     )
     session = AgentSession(
-        llm=realtime_llm
+        llm=realtime_llm,
+        allow_interruptions=False
     )
 
     await session.start(
         room=ctx.room,
-        agent=Assistant(),
+        agent=agent,
         room_input_options=RoomInputOptions(
             # LiveKit Cloud enhanced noise cancellation
             # - If self-hosting, omit this parameter
             # - For telephony applications, use `BVCTelephony` for best results
             noise_cancellation=noise_cancellation.BVC(),
+            audio_enabled=False
         ),
     )
 
     await ctx.connect()
+
+    @session.on("user_state_changed")
+    def on_user_state_changed(event):
+        if event.new_state == "away" and agent.wake_word_detected:
+            print(event)
+            asyncio.create_task(agent.user_timeout())
+            print(f"\nAgent Session ended for inactivity")
+            print("=" * 20)
+            print("Chat History:")
+            for item in session.history.items:
+                if item.type == "message":
+                    text = f"{item.role}: {item.text_content}"
+                    if item.interrupted:
+                        text += " (interrupted)"
+                    print(text)
+            print("=" * 20)
 
 
 if __name__ == "__main__":
